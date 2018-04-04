d04857a17d3cd258229e30139c0ba372270c9299
lbat83 <lbz1983@gmail.com>
2018-04-04 17:15:12 -0500

    scratch

---
+++ NotesAPI/Notes_API/Controllers/NotesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes_API.Entities;
+
+namespace Notes_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Notes")]
+    public class NotesController : Controller
+    {
+        private readonly NotesContext _context;
+
+        public NotesController(NotesContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Notes
+        [HttpGet]
+        public IEnumerable<Notes> GetAll()
+        {
+            return _context.Notes.ToList();
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}", Name = "GetNotes")]
+        public IActionResult GetById(int id)
+        {
+            var item = _context.Notes.FirstOrDefault(n => n.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+        
+        // POST: api/Notes
+        [HttpPost]
+        public IActionResult Post([FromBody] Notes item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _context.Notes.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetNotes", new { id = item.Id }, item);
+        }
+        
+        // PUT: api/Notes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NotesAPI/Notes_API/Entities/Notes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes_API.Entities
+{
+    public class Notes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int CategoryId { get; set; }
+        public int IsDeleted { get; set; }
+        public int UserId { get; set; }
+
+        public Category Category { get; set; }
+        public User User { get; set; }
+    }
+}
